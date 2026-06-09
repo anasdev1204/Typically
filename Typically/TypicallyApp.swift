5e08d3d5431b90e24a6c1f132340190247443519
anasdev1204 <anasdev1204@gmail.com>
2026-06-09 12:20:53 +0100

    chore: init

---
+++ Typically/TypicallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypicallyApp.swift
+//  Typically
+//
+//  Created by Mohamed Anas Abbouchi on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TypicallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
